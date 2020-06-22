2f8351c9e659007ea2970cb54df8092fe4c1131d
yangdepp <yangdepp@163.com>
2020-06-22 21:57:37 +0800

    自定义hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import Hello from './components/demo/Hello';
 import LikeButton from './components/demo/LikeButton';
-import MouseTracker from './components/demo/MouseTracker';
+// import MouseTracker from './components/demo/MouseTracker';
 import useMousePosition from './components/hooks/useMousePosition';
+import useURLLoader from './components/hooks/useURLLoader';
 import './App.css';
+
+interface IShowResult {
+  message: string;
+  status: string;
+}
 
 function App() {
   const [show, setShow] = useState(true);
   const positions = useMousePosition();
+  const [data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random', [show]);
+  const dogResult = data as IShowResult;
   return (
     <div className="App">
       <header className="App-header">
@@ -25,6 +33,11 @@ function App() {
         >
           Toggle Tracker
         </button>
+        {loading ? (
+          <p>🐶 is loading...</p>
+        ) : (
+          <img style={{ width: '200px', height: '200px' }} src={dogResult && dogResult.message} />
+        )}
         {/* {show && <MouseTracker />} */}
         <a
           className="App-link"
