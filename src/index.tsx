6a295faba015d0dcf39d76bd85f974bd7cc8ae65
yangdepu <yangdepp@163.com>
2020-07-14 14:05:07 +0800

    change dir

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,6 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-library.add(fas);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.scss';
+import App from './App';
 
-export { default as Button } from './components/Button';
-export { default as Menu } from './components/Menu';
-export { default as AutoComplete } from './components/AutoComplete';
-export { default as Icon } from './components/Icon';
-export { default as Input } from './components/Input';
-export { default as Progress } from './components/Progress';
-export { default as Transition } from './components/Transition';
-export { default as Upload } from './components/Upload';
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/lib/index.tsx
@@ -0,0 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
+
+export { default as Button } from '../lib/components/Button';
+export { default as Menu } from '../lib/components/Menu';
+export { default as AutoComplete } from '../lib/components/AutoComplete';
+export { default as Icon } from '../lib/components/Icon';
+export { default as Input } from '../lib/components/Input';
+export { default as Progress } from '../lib/components/Progress';
+export { default as Transition } from '../lib/components/Transition';
+export { default as Upload } from '../lib/components/Upload';
