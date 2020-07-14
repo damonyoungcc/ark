a5bc814fffd3b95792e9936e1b8df87839dc8d30
yangdepu <yangdepp@163.com>
2020-07-14 14:10:07 +0800

    static-build

---
+++ build/precache-manifest.5997eb02aad5b144a86dcc036c48ae07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4455e643cb5c80f5cedaaf42f2a657e7",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "45a89cfe3831a3f6a033",
+    "url": "/build/static/css/main.dd8fa1ed.chunk.css"
+  },
+  {
+    "revision": "2ed94a65464e241af3a4",
+    "url": "/build/static/js/2.dc11b07f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/build/static/js/2.dc11b07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a89cfe3831a3f6a033",
+    "url": "/build/static/js/main.dfaed751.chunk.js"
+  },
+  {
+    "revision": "17387629c319061fd3d7",
+    "url": "/build/static/js/runtime-main.d697efbd.js"
+  }
+]);
