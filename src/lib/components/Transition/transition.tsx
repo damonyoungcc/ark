26d384d4da5ea4882bd0385f78423f9247036309
yangdepp <yangdepp@163.com>
2020-07-16 00:51:47 +0800

    add header

--- src/lib/components/Transition/transition.tsx
+++ src/lib/components/Transition/transition.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { CSSTransitionProps } from 'react-transition-group/CSSTransition';
 
-type AnimationName = 'zoom-in-top' | 'zoom-in-left' | 'zoom-in-bottom' | 'zoom-in-right';
+type AnimationName =
+  | 'zoom-in-top'
+  | 'zoom-in-left'
+  | 'zoom-in-bottom'
+  | 'zoom-in-right'
+  | 'zoom-in-center-left'
+  | 'zoom-in-center-right';
 
 type TransitionProps = CSSTransitionProps & {
   animation?: AnimationName;
@@ -12,7 +18,10 @@ type TransitionProps = CSSTransitionProps & {
 const Transition: React.FC<TransitionProps> = (props) => {
   const { children, classNames, animation, wrapper, ...restProps } = props;
   return (
-    <CSSTransition classNames={classNames ? classNames : animation} {...restProps}>
+    <CSSTransition
+      classNames={classNames ? classNames : animation}
+      {...restProps}
+    >
       {wrapper ? <div>{children}</div> : children}
     </CSSTransition>
   );
--- src/page/Header/index.tsx
+++ src/page/Header/index.tsx
@@ -1,16 +1,87 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useRef, KeyboardEvent, ChangeEvent } from 'react';
+import ArkImage from '../../assets/ark.svg';
+import GithubImage from '../../assets/github.svg';
+import { Icon, Transition } from '../../lib';
+import { useClickOutside } from '../../lib';
 import './style.scss';
+const liItems = ['设计', '文档', '组件', '资源', '目的'];
 const App: FC = () => {
+  const [showInput, setShowInput] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const componentRef = useRef<HTMLDivElement>(null);
+  const [selectIndex, setSelectIndex] = useState(2);
+  useClickOutside(componentRef, () => {
+    setShowInput(false);
+    setInputValue('');
+  });
+  const searchSourceCode = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.keyCode !== 13) {
+      return;
+    }
+    if (inputValue.trim()) {
+      window.open(`https://github.com/yangdepp/ark/search?q=${inputValue}`);
+    }
+  };
   return (
     <div className="header-content">
-      <div className="header-logo">LOGO</div>
-      <input type="text" placeholder="在ark源码中搜索"/>
+      <div className="header-logo">
+        <img src={ArkImage} alt="" className="logo-image" />
+        <div>Ark Design</div>
+      </div>
+      <div className="header-search" ref={componentRef}>
+        <Transition
+          in={!showInput}
+          animation="zoom-in-center-right"
+          timeout={0}
+        >
+          <Icon
+            icon="search"
+            className="search-icon"
+            onClick={() => setShowInput(!showInput)}
+          />
+        </Transition>
+        <Transition
+          in={showInput}
+          animation="zoom-in-center-left"
+          timeout={300}
+        >
+          <input
+            autoFocus={showInput}
+            type="text"
+            placeholder="在ark源码中搜索"
+            className="search-input"
+            onKeyDown={searchSourceCode}
+            value={inputValue}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setInputValue(e.target.value)
+            }
+          />
+        </Transition>
+      </div>
       <ul className="header-nav">
-        <li>设计</li>
-        <li>文档</li>
-        <li>组件</li>
-        <li>资源</li>
-        <li>国内镜像</li>
+        {liItems.map((item, index) => (
+          <li
+            key={item}
+            className={`li-item ${index === selectIndex ? 'active' : ''}`}
+            onClick={() => setSelectIndex(index)}
+          >
+            {item}
+          </li>
+        ))}
+        <li
+          className="li-item-repo"
+          onClick={() => window.open('https://github.com/yangdepp/ark')}
+        >
+          <img
+            alt="GitHub Repo"
+            src={GithubImage}
+            style={{ width: '20px', marginRight: '5px' }}
+          />
+          <img
+            alt="GitHub package.json version"
+            src="https://img.shields.io/github/package-json/v/yangdepp/ark?style=social"
+          />
+        </li>
       </ul>
     </div>
   );
